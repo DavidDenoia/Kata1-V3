7448bcaece7a2ce6b667f1c12b58f6917530be18
David <David@DESKTOP-S4EGEPD>
2022-09-16 12:12:00 +0100

    Kata1 V1

---
+++ src/main/java/Kata1/Kata1.java
@@ -0,0 +1,13 @@
+package Kata1;
+
+import java.util.Date;
+
+public class Kata1 {
+
+    public static void main(String[] args) {
+        
+        Person person= new Person("Blas", new Date(49,8,24));
+        System.out.println(person.getName() + " tiene " + person.getAge());
+    }
+    
+}
